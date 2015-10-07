e59a05c3e88e55451bf945289f5560f34c227e1e
Leo <leolin@me.com>
2015-10-07 11:27:55 +0800

    update

--- gulpfile.js
+++
@@ -1,1 +0,0 @@
-var gulp=require("gulp");gulp.task("default",function(){var e=require("gulp-postcss"),r=require("gulp-sourcemaps"),p=require("autoprefixer-core");return gulp.src("./css-prefix/*.css").pipe(r.init()).pipe(e([p({browsers:["last 2 versions"]})])).pipe(r.write(".")).pipe(gulp.dest("./css"))});
--- js/effect.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$("div.mdl-card, .b-lazy").lazyload({effect:"fadeIn",container:$(".mdl-grid")})});
